2fde6ce44304e13eada35a8873a0c45909039c01
huikaihoo <huikaihoo@gmail.com>
2018-11-10 11:42:27 +0800

    Updates

---
+++ precache-manifest.158407fb132767d32b6ff2a81dcad989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3ac2bcc4caf002aac8",
+    "url": "/wt/static/css/main.6b4e6e41.chunk.css"
+  },
+  {
+    "revision": "6e3ac2bcc4caf002aac8",
+    "url": "/wt/static/js/main.6e3ac2bc.chunk.js"
+  },
+  {
+    "revision": "1609e03e2bb3f92055a9",
+    "url": "/wt/static/css/1.42f5b00b.chunk.css"
+  },
+  {
+    "revision": "1609e03e2bb3f92055a9",
+    "url": "/wt/static/js/1.1609e03e.chunk.js"
+  },
+  {
+    "revision": "0f4849ae00fe60f44560",
+    "url": "/wt/static/js/runtime~main.0f4849ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "88fe0498bd73035faa3a643803636048",
+    "url": "/wt/index.html"
+  }
+];
